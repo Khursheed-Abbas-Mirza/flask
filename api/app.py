9ab8ad8c5e14ea5cb5c4f90a763dd1d774d7e19e
Khursheed Abbas <mirzakhursheedabbas14@gmail.com>
2024-12-30 22:33:21 +0530

    done

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
 app=Flask(__name__, static_folder="static", template_folder="templates")
-@app.route('/')
+@app.route('/',methods=["GET"])
 def func():
     return render_template("index.html")
 if(__name__=="__main__"):
