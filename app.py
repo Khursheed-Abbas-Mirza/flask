7f7211b0f34d1790988c60e1d87fc9a1d8411661
Khursheed Abbas <mirzakhursheedabbas14@gmail.com>
2024-12-30 20:05:15 +0530

    commits are done by me

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,send_from_directory
+app=Flask(__name__)
+@app.route('/')
+def func():
+    return render_template("index.html")
+# @app.route('/about')
+# def about():
+#     name="chotu"
+#     return render_template('about.html',name="chotu")
+# @app.route('/static/<path:filename>')
+# def serve_static_file(filename):
+#     print(filename)
+#     return send_from_directory(app.static_folder, filename)
+app.run(host="localhost",port=3036,debug=True)
